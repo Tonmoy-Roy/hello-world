d19f26df6542f1a8b1fd970ac18bb4efb902f785
Tonmoy Roy <60349582+Tonmoy-Roy@users.noreply.github.com>
2020-01-28 09:41:25 -0800

    Add files via upload

---
+++ mirror matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a[100][100],i,r,c,n,j;
+    scanf("%d%d",&r,&c);
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+            scanf("%d",&a[i][j]);
+    }
+    for(i=0; i<r; i++)
+        for(j=0; j<c; j++)
+    {
+            printf("\n");
+        for(j=c-1; j>=0; j--)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+        break;
+    }
+    return 0;
+}
